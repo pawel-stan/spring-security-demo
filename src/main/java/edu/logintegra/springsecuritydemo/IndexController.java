2226f6cc9002838ae0b5f8900e1ddb369e836ae2
Paweł Staniszewski <pawel.staniszewski@logintegra.com>
2021-04-09 17:14:56 +0200

    Strona kontaktowa dostępna dla niezalogowanych użytkowników

--- src/main/java/edu/logintegra/springsecuritydemo/IndexController.java
+++ src/main/java/edu/logintegra/springsecuritydemo/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
 }
