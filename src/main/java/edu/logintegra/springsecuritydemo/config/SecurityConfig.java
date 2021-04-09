0b4f4174f71c040ff8dd7ad8f5d66608c472d55f
Paweł Staniszewski <pawel.staniszewski@logintegra.com>
2021-04-10 00:10:01 +0200

    Formularz logowania oraz i18n

--- src/main/java/edu/logintegra/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/edu/logintegra/springsecuritydemo/config/SecurityConfig.java
@@ -19,6 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/contact").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll();
     }
 }
