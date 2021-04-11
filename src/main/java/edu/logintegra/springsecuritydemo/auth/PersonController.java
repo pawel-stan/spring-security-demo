e854d77db98c28ae8ea46a481c93f1c14a28047b
Paweł Staniszewski <pawel.staniszewski@logintegra.com>
2021-04-11 02:33:33 +0200

    Przygotowuje listę użytkowników

--- src/main/java/edu/logintegra/springsecuritydemo/auth/PersonController.java
+++ src/main/java/edu/logintegra/springsecuritydemo/auth/PersonController.java
@@ -4,14 +4,23 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/people")
 public class PersonController {
 
+    private final PersonRepository personRepository;
+
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     @GetMapping("/")
     @Secured("ROLE_USERS_TAB")
-    public String index() {
-        return "people/index";
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("people/index");
+        modelAndView.addObject("people", personRepository.findAll());
+        return modelAndView;
     }
 }
